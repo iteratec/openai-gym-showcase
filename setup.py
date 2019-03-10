4ef496237d4d675715e2bd2d5be306a40b8488cc
Danny Lade <danny.lade@iteratec.de>
2019-03-10 21:43:22 +0100

    Updated used gym version.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from os import path
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
-def read(fname):
-    return open(path.join(path.dirname(__file__), fname)).read()
+def __readme__():
+    return open(path.join(path.dirname(__file__), 'README.md')).read()
 
 
 setup(
@@ -12,21 +12,25 @@ setup(
     author="Danny Lade",
     author_email="dannylade@gmail.com",
     description=("Some showcases for 'OpenAI Gym' based on Python 3.5."),
-    license="LGPL",
+    license='GPLv3',
     keywords="openai gym showcase example",
     url="https://github.com/iteratec/openai-gym-showcase",
-    location="https://github.com/iteratec/openai-gym-showcase",
-    long_description=read('README.md'),
+    long_description=__readme__(),
+    long_description_content_type='text/markdown',
 
     install_requires=[
         'python_version>="3.5"',
-        'gym[all]>=0.10.8',
+        'gym[atari]>=0.12.0',
+        'gym[box2d]>=0.12.0',
+        'gym[classic_control]>=0.12.0',
     ],
 
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Games/Entertainment :: Simulation",
         "Programming Language :: Python :: 3.5",
-        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
 )
